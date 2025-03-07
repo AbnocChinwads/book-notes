ea69f0845c0e9f0ea3dff29d08fbfc40ea78f8fa
AbnocChinwads <rnemo@hotmail.co.uk>
2025-03-07 20:39:39 +0000

    Adjusted index.js to call dotenv config with best practice in mind

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 const port = process.env.APP_PORT;
+
+// Init Database
 
 const db = new pg.Client({
   user: process.env.DB_USER,
@@ -17,8 +17,10 @@ const db = new pg.Client({
 });
 db.connect();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); // Allows us to pass webpage information to the server
+app.use(express.static("public")); // Allows use of static files with expressjs
+
+// Initial call to display main page
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
